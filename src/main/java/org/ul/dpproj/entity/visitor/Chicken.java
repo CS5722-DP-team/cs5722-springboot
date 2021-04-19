5f8747d24e2b211cf346e1d3bcf327562726dad3
JackieYU233 <yujunke1204@163.com>
2021-04-19 16:19:01 +0800

    visitor

--- src/main/java/org/ul/dpproj/entity/visitor/Chicken.java
+++ src/main/java/org/ul/dpproj/entity/visitor/Chicken.java
@@ -2,6 +2,16 @@ package org.ul.dpproj.entity.visitor;
 
 public class Chicken implements BurgerPart{
 
+    private int size;
+
+    public void Chichen(int size){
+        this.size = size;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
     @Override
     public void accept(BurgerPartVisitor burgerPartVisitor) {
         burgerPartVisitor.visit(this);
